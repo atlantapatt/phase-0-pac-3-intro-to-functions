38f1b3554913b3c59c444722276f8d2af0406912
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-02-23 14:50:38 -0800

    commit tree 3489b0c49461bacbea6a5b93aea5411dde9d5635

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Follow along with the examples here
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!")
+}
+sayHelloToGuadalupe()
+function sayHelloToLiz() {
+    console.log("Hello, Liz!")
+}
+sayHelloToLiz()
+function sayHelloToSamip() {
+    console.log("Hello Samip!")
+}
+sayHelloToSamip()
+function sayHelloTo(firstName) {
+    console.log (`Hello, ${firstName}!`)
+}
+function say(greeting, firstName) {
+   console.log(`${greeting}, ${firstName}!`)
+}
+say("Goodbye", "Julio")
+say("Julio", "hello")
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName)
+    console.log("greeting: " , greeting)
+    console.log(`${greeting}, ${firstName}!`)
+}
+
